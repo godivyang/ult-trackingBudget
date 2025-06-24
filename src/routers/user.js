61734dbf95eb6728a660049dfb4d1a970291c747
godivyang <godivyang@users.noreply.github.com>
2025-06-24 19:14:27 +0530

    SSO feature implementation part 6

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = new express.Router();
 const auth = require("../middleware/auth");
 
+router.post("/user/me", auth, async (req, res) => {
+    res.send(req.user);
+});
+
 router.get("/", async (req, res) => {
     res.send("Welcome to Tracking Budget app!");
 });
