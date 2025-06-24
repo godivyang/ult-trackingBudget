38039661ea790488dd94dd2907153940ae4b2c82
godivyang <godivyang@users.noreply.github.com>
2025-06-24 23:06:34 +0530

    Cross app login final changes part 4

--- src/routers/user.js
+++ src/routers/user.js
@@ -4,12 +4,13 @@ const auth = require("../middleware/auth");
 
 const tokenOptions = {
     httpOnly: true,
-    secure: process.env.cookie_Secure == "true",
-    sameSite: process.env.cookie_SameSite,
+    secure: process.env.COOKIE_SECURE == "true",
+    sameSite: process.env.COOKIE_SAME_SITE,
     maxAge: 7 * 24 * 60 * 60 * 1000
 };
 
 router.post("/user/me", auth, async (req, res) => {
+    console.log("token", req.token);
     res.cookie("token", req.token, tokenOptions);
     res.send(req.user);
 });
