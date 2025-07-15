68ab12aa20acbc500aa806ccf271d83c40c83d8b
godivyang <godivyang@users.noreply.github.com>
2025-07-15 17:29:23 +0530

    CORS error fix

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const allowedOrigin = process.env.TRACKING_BUDGET_FRONTEND_URL;
 app.use(cors({
     origin: function (origin, callback) {
         // Allow requests with no origin (like mobile apps, curl, or Postman)
-        // console.log(origin);
+        console.log(origin);
         if(!origin) return callback(null, true);
 
         if(allowedOrigin === origin) {
