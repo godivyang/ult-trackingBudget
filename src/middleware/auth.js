92992d1b1e4fcf4ed72fda80455c1be98e48d50a
godivyang <godivyang@users.noreply.github.com>
2025-07-15 22:45:23 +0530

    SSO error resolution 1

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -48,7 +48,6 @@ const checkIfValidToken = async (token) => {
         // console.log(response.data)
         return response.data;
     } catch (e) {
-        console.log(e)
         return undefined;
     }
 }
